c1835eba4a81b968dfd1e685b6c0d4a3f9d22927
xyphertron <sarveshlanjewar@Sarveshs-MacBook-Air.local>
2024-01-27 11:33:58 +0530

    Folder Upload

---
+++ WebAPI/Migrations/20240125080755_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DCandidates",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    fullName = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    mobile = table.Column<string>(type: "nvarchar(16)", nullable: false),
+                    email = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    age = table.Column<int>(type: "int", nullable: false),
+                    bloodGroup = table.Column<string>(type: "nvarchar(3)", nullable: false),
+                    address = table.Column<string>(type: "nvarchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DCandidates", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DCandidates");
+        }
+    }
+}
---
+++ WebAPI/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DonationDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection"))
+);
+
+builder.Services.AddCors();
+
+
+var app = builder.Build();
+
+app.UseCors(options =>
+            options.WithOrigins("http://localhost:3002")
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    // app.UseCors(options =>
+    //         options.WithOrigins("http://localhost:3002")
+    //         .AllowAnyHeader()
+    //         .AllowAnyMethod());
+}
+
+// app.UseCors(options =>
+//             options.WithOrigins("http://localhost:3002")
+//             .AllowAnyHeader()
+//             .AllowAnyMethod());
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
